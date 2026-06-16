e2d891bcc399971044ab834c4c1d90f6004ac1d3
shubham <tiwarishubh723@gmail.com>
2026-06-16 19:28:11 +0530

    implement matches POST AND GET ROUTES

---
+++ routes/matches.js
@@ -0,0 +1,67 @@
+import { Router } from 'express'
+import { createMatchSchema, listMatchesQuerySchema } from '../src/validation/matches.js'
+import { db } from '../src/db.js'
+import { matches } from '../src/schema.js'
+import { getMatchStatus } from '../src/utils/match-status.js'
+import { parse } from 'dotenv'
+import { de } from 'zod/locales'
+import { desc } from 'drizzle-orm'
+
+const matchRouter = Router()
+const Max_LIMIT = 100
+matchRouter.get('/', async(req, res) => {
+    const parsed = listMatchesQuerySchema.safeParse(req.query)
+
+    if(!parsed.success){
+        res.status(400).json({err: 'invalid ' ,details : parse.error})
+    }
+
+    const limit = Math.min(parsed.data.limit ?? 50 , Max_LIMIT)
+
+    try{
+        const data = await db
+          .select()
+          .from(matches)
+          .orderBy((desc(matches.createdAt)))
+          .limit(limit)
+
+          res.json({data})
+    }catch(err){
+        res.status(500).json({err: 'failed '})
+    }
+ })
+
+matchRouter.post('/', async (req, res) => {
+    const parsed = createMatchSchema.safeParse(req.body)
+   
+  
+    console.log('body:', req.body)         
+    console.log('parsed:', parsed)          
+
+    if (!parsed.success) {
+        return res.status(400).json({ error: 'invalid payload', issues: parsed.error.issues }) //  zod errors
+    }
+
+  const { data: { startTime, endTime, homeScore, awayScore }} = parsed
+
+    try {
+        const [event] = await db
+            .insert(matches)
+            .values({
+                ...parsed.data,
+                startTime: new Date(startTime),
+                endTime : new Date(endTime), 
+                homeScore: homeScore ?? 0 ,
+                awayScore : awayScore ?? 0,
+                status: getMatchStatus(startTime, endTime) 
+            })
+            .returning()
+
+        res.status(201).json({ data: event })
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ error: 'internal server error' })
+    }
+})
+
+export default matchRouter
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-import express from "express"
+import express, { Router } from "express"
+import  matchRouter  from '../routes/matches.js'
 const app = express()
 const port = 8000
 app.use(express.json())
@@ -6,6 +7,12 @@ app.use(express.json())
 app.get('/', (req, res)=>{
     res.send('helloo from express')
 })
+
+app.use('/matches',matchRouter)
+
+
 app.listen(port, ()=>{
     console.log(`server is listening to http://localhost:${port}`)
 })
+
+
